341e7d19420b445d853564256d1787c274cc4490
Andrei Markeev <andrei-markeev@users.noreply.github.com>
2022-04-24 06:29:09 +0300

    token cache

--- README.md
+++ README.md
@@ -39,6 +39,9 @@ http {
 }
 ```
 
+_Note_: even though Lua blocks are used in most configuration examples, this module is not dependent on Openresty.
+You can use it with a bare Nginx as well.
+
 ## How to generate keys and create JWT tokens
 
 See [test/README.md](test/README.md).
@@ -78,6 +81,48 @@ end
 
 _Note_: JWT parsing works in access phase, so `$jwt_claims` variable is only available after that.
 
+## Token cache
+
+Typically, JWT token is valid for at least one hour, so if we enable token caching, it can dramatically improve the verification performance in most real-world scenarios.
+
+You can enable the caching by using `auth_jwt_cache` directive:
+
+```nginx
+http {
+    auth_jwt_cache on;
+    auth_jwt_key 'Base64EncodedPublicKey==';
+
+    server {
+        listen 127.0.0.1:80;
+        server_name localhost;
+
+        location = /admin {
+            auth_jwt on;
+            content_by_lua_block {
+                ngx.say('Welcome to Admin area!')
+            }
+        }
+    }
+}
+```
+
+By default, 256KB of memory will be used for caching.
+How many tokens it will fit into the cache, depends on how big your token is.
+
+For example, for this header and payload:
+```
+Header: {"alg":"EdDSA","crv":"Ed25519","typ":"JWT"}
+Payload: {"sub":"12345678-abcd-1234-dcba-1234567890ab","exp":1651000000000,"iss":"https://www.test.com"}
+```
+
+The token size will be 273 bytes, and we have to round that up to nearest power of 2 (in this case, to 512 bytes) because of Nginx memory allocation rules, so 256Kb will fit approximately 512 tokens of 512 bytes each. Usually you should calculate the size of the cache according to the expected amount of the logged in users.
+
+You can modify cache settings by using `auth_jwt_cache` directive parameter `size`:
+
+```nginx
+auth_jwt_cache on size=1m;
+```
+
 ## Directives
 
 ### auth_jwt
@@ -103,7 +148,8 @@ If the `Authorization` header is empty, contains invalid JWT token or the signat
 
 **Note**: This library doesn't parse the token payload and doesn't perform any claims checks, it only puts the claims JSON into `$jwt_claims` variable. For example, it doesn't validate `exp` claim, you have to do it yourself if needed.
 
-**Note 2**: Only **EdDSA** algorithm with _Ed25519_ curve is supported and also enforced, value of `alg` specified in the header is ignored.
+**Note 2**: Only **EdDSA** algorithm with _Ed25519_ curve is currently supported and also enforced, value of `alg` specified in the header is
+ignored.
 
 ### auth_jwt_key
 
@@ -121,6 +167,21 @@ openssl pkey -in private_key.pem -noout -text_pub | sed 1,2d | tr -d '\n\r :' | 
 
 Notice that using a key encoded in DER ASN.1 format (i.e. starting with `-----BEGIN PUBLIC KEY-----`) is **not supported** with this directive. You need to use the _value_ of the public key, and encode it in _base64_ (it is often encoded in _hex_ format).
 
+### auth_jwt_cache
+
+**syntax**: _auth_jwt_cache on|off [size=**(size-in-bytes)**]_
+
+**default**: _auth_jwt off size=256k_
+
+**context**: http
+
+Configures the token cache. If enabled, bearer tokens that were extracted from `Authorization` header, will be cached in order to improve verification performance.
+
+`size` parameter specifies, how much memory will be used for cache in bytes. You can use `k` and `m` prefixes for convenience, e.g. `1k` = `1000` bytes. `size` must be more or equal to the size of one memory page, which is usually `4096` bytes.
+
+**Note**: In addition to the requested `size`, at least 4 pages of memory (i.e. 16Kb) will be allocated because of the data structures overhead.
+
+**Note 2**: At this point, only valid tokens are cached.
 
 ## Contributions
 
--- ngx_http_jwted_module.c
+++ ngx_http_jwted_module.c
@@ -6,10 +6,24 @@
 
 static ngx_int_t ngx_http_jwted_pre_conf(ngx_conf_t *cf);
 static ngx_int_t ngx_http_jwted_post_conf(ngx_conf_t *cf);
+static void *ngx_http_jwted_create_main_conf(ngx_conf_t *cf);
+static char *ngx_http_jwted_init_main_conf(ngx_conf_t *cf, void *conf);
 static void *ngx_http_jwted_create_loc_conf(ngx_conf_t *cf);
 static char *ngx_http_jwted_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
+static char *ngx_http_jwted_set_cache(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static ngx_int_t ngx_http_jwted_cache_zone_init(ngx_shm_zone_t *shm_zone, void *data);
 static ngx_int_t ngx_http_jwted_handler(ngx_http_request_t *req);
+static ngx_int_t ngx_http_jwted_expose_claims(ngx_http_request_t *req, ngx_str_t token);
 static ngx_int_t ngx_http_jwted_claims_getter(ngx_http_request_t *req, ngx_http_variable_value_t *value, uintptr_t var_name);
+static ngx_int_t ngx_http_jwted_cache_token(ngx_http_request_t *req, ngx_str_t token);
+static void ngx_http_jwted_free_cache_locked(ngx_queue_t *cache_expire_queue, ngx_rbtree_t *cache_tree, ngx_slab_pool_t *shpool);
+
+typedef struct
+{
+    ngx_flag_t cache_enabled;
+    ssize_t cache_size;
+    ngx_shm_zone_t *shm_zone;
+} ngx_http_jwted_main_conf_t;
 
 typedef struct
 {
@@ -21,6 +35,19 @@ typedef struct
 {
     ngx_str_t claims_json;
 } ngx_http_jwted_data_t;
+
+typedef struct
+{
+    ngx_rbtree_t cache_tree;
+    ngx_rbtree_node_t cache_tree_sentinel;
+    ngx_queue_t cache_expire_queue;
+} ngx_http_jwted_shared_data_t;
+
+typedef struct
+{
+    ngx_str_node_t *str_node;
+    ngx_queue_t queue_item;
+} ngx_http_jwted_queue_data_t;
 
 static ngx_command_t ngx_http_jwted_commands[] = {
     {ngx_string("auth_jwt"),
@@ -35,13 +62,19 @@ static ngx_command_t ngx_http_jwted_commands[] = {
      NGX_HTTP_LOC_CONF_OFFSET,
      offsetof(ngx_http_jwted_loc_conf_t, public_key),
      NULL},
+    {ngx_string("auth_jwt_cache"),
+     NGX_HTTP_MAIN_CONF | NGX_CONF_TAKE12,
+     ngx_http_jwted_set_cache,
+     NGX_HTTP_MAIN_CONF_OFFSET,
+     0,
+     NULL},
     ngx_null_command};
 
 static ngx_http_module_t ngx_http_jwted_module_ctx = {
     ngx_http_jwted_pre_conf,
     ngx_http_jwted_post_conf,
-    NULL, // create main configuration
-    NULL, // init main configuration
+    ngx_http_jwted_create_main_conf,
+    ngx_http_jwted_init_main_conf,
     NULL, // create server configuration
     NULL, // merge server configuration
     ngx_http_jwted_create_loc_conf,
@@ -64,7 +97,7 @@ ngx_module_t ngx_http_jwted_module = {
 ngx_str_t var_name_jwt_claim = ngx_string("jwt_claims");
 static ngx_int_t ngx_http_jwted_pre_conf(ngx_conf_t *cf)
 {
-    ngx_http_variable_t* var = ngx_http_add_variable(cf, &var_name_jwt_claim, 0);
+    ngx_http_variable_t *var = ngx_http_add_variable(cf, &var_name_jwt_claim, 0);
     if (var == NULL)
         return NGX_ERROR;
     var->get_handler = ngx_http_jwted_claims_getter;
@@ -75,18 +108,123 @@ static ngx_int_t ngx_http_jwted_pre_conf(ngx_conf_t *cf)
 
 static ngx_int_t ngx_http_jwted_post_conf(ngx_conf_t *cf)
 {
-    ngx_http_handler_pt *h;
-    ngx_http_core_main_conf_t *cmcf;
+    ngx_http_core_main_conf_t *core_conf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
 
-    cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
-
-    h = ngx_array_push(&cmcf->phases[NGX_HTTP_ACCESS_PHASE].handlers);
+    ngx_http_handler_pt *h = ngx_array_push(&core_conf->phases[NGX_HTTP_ACCESS_PHASE].handlers);
     if (h == NULL)
         return NGX_ERROR;
 
     *h = ngx_http_jwted_handler;
 
     return NGX_OK;
+}
+
+ngx_str_t cache_zone_name = ngx_string("jwted_cache");
+static char *ngx_http_jwted_set_cache(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+    ngx_http_jwted_main_conf_t *main_conf = conf;
+    ngx_str_t *value = cf->args->elts;
+
+    if (ngx_strcmp(value[1].data, "off") == 0)
+    {
+        main_conf->cache_enabled = 0;
+        return NGX_CONF_OK;
+    }
+
+    if (ngx_strcmp(value[1].data, "on") != 0)
+    {
+        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "First parameter of `auth_jwt_cache` must be either \"on\" or \"off\"");
+        return NGX_CONF_ERROR;
+    }
+
+    main_conf->cache_enabled = 1;
+    main_conf->cache_size = ngx_align(256 * 1024, ngx_pagesize);
+
+    ngx_uint_t i;
+    ngx_str_t s;
+    for (i = 2; i < cf->args->nelts; i++)
+    {
+
+        if (ngx_strncmp(value[i].data, "size=", 5) == 0)
+        {
+            s.data = value[i].data + 5;
+            s.len = value[i].len - 5;
+            main_conf->cache_size = ngx_parse_size(&s);
+
+            if (main_conf->cache_size < (ssize_t)ngx_pagesize)
+            {
+                ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "Invalid `auth_jwt_cache` size \"%V\"! Should be >=%d bytes", &value[i], ngx_pagesize);
+                return NGX_CONF_ERROR;
+            }
+
+            main_conf->cache_size = 4 * ngx_pagesize + ngx_align(main_conf->cache_size, ngx_pagesize);
+            continue;
+        }
+
+        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "Invalid parameter \"%V\"! Expected \"size=<size>\"", &value[i]);
+        return NGX_CONF_ERROR;
+    }
+
+    ngx_shm_zone_t *shm_zone = ngx_shared_memory_add(cf, &cache_zone_name, main_conf->cache_size, &ngx_http_jwted_module);
+    if (shm_zone == NULL)
+    {
+        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "Error adding shared memory zone for `auth_jwt_cache`");
+        return NGX_CONF_ERROR;
+    }
+
+    shm_zone->init = ngx_http_jwted_cache_zone_init;
+
+    main_conf->shm_zone = shm_zone;
+
+    return NGX_CONF_OK;
+}
+
+static ngx_int_t ngx_http_jwted_cache_zone_init(ngx_shm_zone_t *shm_zone, void *data)
+{
+    ngx_slab_pool_t *shpool = (ngx_slab_pool_t *)shm_zone->shm.addr;
+    if (shm_zone->shm.exists)
+    {
+        shm_zone->data = shpool->data;
+        return NGX_OK;
+    }
+
+    ngx_http_jwted_shared_data_t *shared_data = ngx_slab_alloc(shpool, sizeof(ngx_http_jwted_shared_data_t));
+    if (!shared_data)
+        return NGX_ERROR;
+
+    ngx_rbtree_init(&shared_data->cache_tree, &shared_data->cache_tree_sentinel, ngx_str_rbtree_insert_value);
+
+    ngx_queue_init(&shared_data->cache_expire_queue);
+
+    shm_zone->data = shared_data;
+    shpool->data = shared_data;
+    shpool->log_nomem = 0;
+
+    return NGX_OK;
+}
+
+static void *ngx_http_jwted_create_main_conf(ngx_conf_t *cf)
+{
+    ngx_http_jwted_main_conf_t *conf;
+
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_jwted_main_conf_t));
+    if (conf == NULL)
+        return NULL;
+
+    conf->cache_enabled = NGX_CONF_UNSET;
+    conf->cache_size = NGX_CONF_UNSET_SIZE;
+
+    return conf;
+}
+
+static char *ngx_http_jwted_init_main_conf(ngx_conf_t *cf, void *conf)
+{
+    ngx_http_jwted_main_conf_t *main_conf = conf;
+
+    ngx_conf_init_value(main_conf->cache_enabled, 0);
+    ngx_conf_init_value(main_conf->cache_size, 256 * 1024);
+
+    return NGX_CONF_OK;
 }
 
 static void *ngx_http_jwted_create_loc_conf(ngx_conf_t *cf)
@@ -116,8 +254,8 @@ static char *ngx_http_jwted_merge_loc_conf(ngx_conf_t *cf, void *parent, void *c
 
 static ngx_int_t ngx_http_jwted_handler(ngx_http_request_t *req)
 {
-    ngx_http_jwted_loc_conf_t *conf;
-    conf = ngx_http_get_module_loc_conf(req, ngx_http_jwted_module);
+    ngx_http_jwted_main_conf_t *main_conf = ngx_http_get_module_main_conf(req, ngx_http_jwted_module);
+    ngx_http_jwted_loc_conf_t *conf = ngx_http_get_module_loc_conf(req, ngx_http_jwted_module);
 
     if (!conf->flag)
         return NGX_DECLINED;
@@ -151,11 +289,27 @@ static ngx_int_t ngx_http_jwted_handler(ngx_http_request_t *req)
     payload.data = token.data;
     payload.len = p_last_dot - token.data;
 
+    if (main_conf->cache_enabled)
+    {
+        uint32_t hash = ngx_crc32_long(token.data, token.len);
+        ngx_http_jwted_shared_data_t *shared_data = (ngx_http_jwted_shared_data_t *)main_conf->shm_zone->data;
+        ngx_slab_pool_t *shpool = (ngx_slab_pool_t *)main_conf->shm_zone->shm.addr;
+        ngx_shmtx_lock(&shpool->mutex);
+        ngx_str_node_t *found_node = ngx_str_rbtree_lookup(&shared_data->cache_tree, &token, hash);
+        ngx_shmtx_unlock(&shpool->mutex);
+        if (found_node != NULL)
+        {
+            ngx_http_jwted_expose_claims(req, payload);
+            return NGX_DECLINED;
+        }
+    }
+
     ngx_str_t signature;
     signature.data = p_last_dot + 1;
     signature.len = token.len - payload.len - 1;
 
-    if (conf->public_key.len == 0) {
+    if (conf->public_key.len == 0)
+    {
         ngx_log_error(NGX_LOG_ERR, req->connection->log, 0, "Public key was not specified! Please use `auth_jwt_key`");
         return NGX_HTTP_UNAUTHORIZED;
     }
@@ -228,6 +382,14 @@ static ngx_int_t ngx_http_jwted_handler(ngx_http_request_t *req)
     EVP_PKEY_free(pkey);
     EVP_MD_CTX_free(ctx);
 
+    if (main_conf->cache_enabled)
+        ngx_http_jwted_cache_token(req, token);
+
+    return ngx_http_jwted_expose_claims(req, payload);
+}
+
+static ngx_int_t ngx_http_jwted_expose_claims(ngx_http_request_t *req, ngx_str_t payload)
+{
     u_char *p_dot = ngx_strlchr(payload.data, payload.data + payload.len, '.');
     if (p_dot == NULL)
         return NGX_HTTP_UNAUTHORIZED;
@@ -245,7 +407,7 @@ static ngx_int_t ngx_http_jwted_handler(ngx_http_request_t *req)
         return NGX_HTTP_INTERNAL_SERVER_ERROR;
     }
 
-    decode_result = ngx_decode_base64url(&decoded_claims, &claims);
+    ngx_int_t decode_result = ngx_decode_base64url(&decoded_claims, &claims);
     if (decode_result == NGX_ERROR)
     {
         ngx_log_error(NGX_LOG_ERR, req->connection->log, 0, "Failed to decode claims part of the token, even though signature verified");
@@ -253,7 +415,7 @@ static ngx_int_t ngx_http_jwted_handler(ngx_http_request_t *req)
     }
 
     ngx_http_jwted_data_t *module_ctx_data = ngx_pcalloc(req->pool, sizeof(*module_ctx_data));
-    if (ctx == NULL)
+    if (module_ctx_data == NULL)
     {
         ngx_log_error(NGX_LOG_ERR, req->connection->log, 0, "Failed to allocate module context");
         return NGX_HTTP_INTERNAL_SERVER_ERROR;
@@ -279,4 +441,76 @@ static ngx_int_t ngx_http_jwted_claims_getter(ngx_http_request_t *req, ngx_http_
     value->valid = 1;
 
     return NGX_OK;
+}
+
+#define ngx_slab_alloc_locked_with_retry(shpool, shared_data, var, size) \
+    var = ngx_slab_alloc_locked(shpool, size); \
+    if (var == NULL) \
+    { \
+        ngx_http_jwted_free_cache_locked(&shared_data->cache_expire_queue, &shared_data->cache_tree, shpool); \
+        var = ngx_slab_alloc_locked(shpool, size); \
+    }
+
+static ngx_int_t ngx_http_jwted_cache_token(ngx_http_request_t *req, ngx_str_t token)
+{
+    ngx_http_jwted_main_conf_t *main_conf = ngx_http_get_module_main_conf(req, ngx_http_jwted_module);
+    ngx_http_jwted_shared_data_t *shared_data = (ngx_http_jwted_shared_data_t *)main_conf->shm_zone->data;
+    ngx_slab_pool_t *shpool = (ngx_slab_pool_t *)main_conf->shm_zone->shm.addr;
+
+    ngx_shmtx_lock(&shpool->mutex);
+    ngx_str_node_t *str_node;
+    ngx_slab_alloc_locked_with_retry(shpool, shared_data, str_node, sizeof(ngx_str_node_t));
+    if (str_node == NULL)
+    {
+        ngx_shmtx_unlock(&shpool->mutex);
+        ngx_log_error(NGX_LOG_ERR, req->connection->log, 0, "Failed to allocate memory for cache node");
+        return NGX_ERROR;
+    }
+    uint32_t hash = ngx_crc32_long(token.data, token.len);
+    str_node->node.key = hash;
+    ngx_slab_alloc_locked_with_retry(shpool, shared_data, str_node->str.data, token.len);
+    if (str_node->str.data == NULL)
+    {
+        ngx_slab_free_locked(shpool, str_node);
+        ngx_shmtx_unlock(&shpool->mutex);
+        ngx_log_error(NGX_LOG_ERR, req->connection->log, 0, "Failed to allocate memory for token cache");
+        return NGX_ERROR;
+    }
+    ngx_memcpy(str_node->str.data, token.data, token.len);
+    str_node->str.len = token.len;
+    ngx_rbtree_insert(&shared_data->cache_tree, &str_node->node);
+
+    ngx_http_jwted_queue_data_t *queue_data;
+    ngx_slab_alloc_locked_with_retry(shpool, shared_data, queue_data, sizeof(ngx_http_jwted_queue_data_t));
+    if (queue_data == NULL)
+    {
+        ngx_slab_free_locked(shpool, str_node->str.data);
+        ngx_slab_free_locked(shpool, str_node);
+        ngx_shmtx_unlock(&shpool->mutex);
+        ngx_log_error(NGX_LOG_ERR, req->connection->log, 0, "Failed to allocate memory for cache queue data");
+        return NGX_ERROR;
+    }
+
+    queue_data->str_node = str_node;
+    ngx_queue_insert_head(&shared_data->cache_expire_queue, &queue_data->queue_item);
+
+    ngx_shmtx_unlock(&shpool->mutex);
+
+    return NGX_OK;
+}
+
+static void ngx_http_jwted_free_cache_locked(ngx_queue_t *cache_expire_queue, ngx_rbtree_t *cache_tree, ngx_slab_pool_t *shpool)
+{
+    if (ngx_queue_empty(cache_expire_queue))
+        return;
+
+    ngx_queue_t *last = ngx_queue_last(cache_expire_queue);
+    ngx_http_jwted_queue_data_t *last_data = ngx_queue_data(last, ngx_http_jwted_queue_data_t, queue_item);
+    ngx_queue_remove(last);
+
+    ngx_rbtree_delete(cache_tree, &last_data->str_node->node);
+
+    ngx_slab_free_locked(shpool, last_data->str_node->str.data);
+    ngx_slab_free_locked(shpool, last_data->str_node);
+    ngx_slab_free_locked(shpool, last_data);
 }
